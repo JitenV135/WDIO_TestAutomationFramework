db92bb6e1dbef47112da20665e0370f58a7b8560
Jiten <jiten.vasant@gmail.com>
2024-04-18 13:21:06 +0200

    Add .env template

--- src/configs/wdio.BROWSERSTACK.conf.ts
+++ src/configs/wdio.BROWSERSTACK.conf.ts
@@ -1,40 +1,37 @@
-import type { Options } from '@wdio/types';
+import { Options } from "@wdio/types";
 
 export const dynamicConfig: Options.Testrunner = {
   user: process.env.BROWSERSTACK_USERNAME,
   key: process.env.BROWSERSTACK_ACCESS_KEY,
-  hostname: 'hub.browserstack.com',
+  hostname: "hub.browserstack.com",
   services: [
-    [
-      'browserstack',
-      { browserstackLocal: false, opts: { forcelocal: false } },
-    ],
+    ["browserstack", { browserstackLocal: false, opts: { forcelocal: false } }],
   ],
   maxInstances: 1,
   capabilities: [
     {
-      browserName: 'Chrome',
-      'bstack:options': {
-        browserVersion: 'latest',
-        os: 'Windows',
-        osVersion: '11',
-      }
+      browserName: "Chrome",
+      "bstack:options": {
+        browserVersion: "latest",
+        os: "Windows",
+        osVersion: "11",
+      },
     },
     {
-      browserName: 'Firefox',
-      'bstack:options': {
-        browserVersion: 'latest',
-        os: 'Windows',
-        osVersion: '11',
-      }
+      browserName: "Firefox",
+      "bstack:options": {
+        browserVersion: "latest",
+        os: "Windows",
+        osVersion: "11",
+      },
     },
     {
-      browserName: 'Safari',
-      'bstack:options': {
-        browserVersion: '17.0',
-        os: 'OS X',
-        osVersion: 'Sonoma'
-      }
+      browserName: "Safari",
+      "bstack:options": {
+        browserVersion: "17.0",
+        os: "OS X",
+        osVersion: "Sonoma",
+      },
     },
   ],
 };
--- src/features/app/APP.test.feature
+++
@@ -1,7 +0,0 @@
-Feature: Test
-  @channelEmailInvit
-  Scenario Outline: Test
-    Given I login to the app as the admin user of a child organization
-    # Given I login to the admin panel as the admin user
-    Then I see the Navbar-Breadcrumb-Quickstart-Link displayed
-    # Then I see the Navbar-Breadcrumb-Organizations-Link displayed
